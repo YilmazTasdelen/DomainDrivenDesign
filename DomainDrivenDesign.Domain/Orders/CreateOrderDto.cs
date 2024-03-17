fe77389add69e31b3335aa451b08854f82820751
yilmaz.tasdelen <yilmaztasdelen1994@gmail.com>
2024-03-17 18:28:16 +0300

    .

---
+++ DomainDrivenDesign.Domain/Orders/CreateOrderDto.cs
@@ -0,0 +1,13 @@
+﻿namespace DomainDrivenDesign.Domain.Orders
+{
+    public sealed partial class Order
+    {
+        public sealed record CreateOrderDto(
+            Guid productId,
+            int quantity,
+            decimal amaunt,
+            string currency
+            );
+    }
+}
+
--- DomainDrivenDesign.Domain/Orders/Order.cs
+++ DomainDrivenDesign.Domain/Orders/Order.cs
@@ -1,4 +1,5 @@
 ﻿using DomainDrivenDesign.Domain.Abstractions;
+using DomainDrivenDesign.Domain.Shared;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,19 +8,50 @@ using System.Threading.Tasks;
 
 namespace DomainDrivenDesign.Domain.Orders
 {
-    public sealed class Order:Entity
+    public sealed partial class Order : Entity
     {
-        public Order(Guid id,string orderNumber, DateTime createDate, OrderStatusEnum status, ICollection<OrderLine> orderLines) : base(id)
+        public Order(Guid id, string orderNumber, DateTime createDate, OrderStatusEnum status) : base(id)
         {
             OrderNumber = orderNumber;
             CreateDate = createDate;
             Status = status;
-            OrderLines = orderLines;
         }
 
-        public string OrderNumber { get;private set; }
+        public string OrderNumber { get; private set; }
         public DateTime CreateDate { get; private set; }
-        public OrderStatusEnum Status {  get; private set; }
+        public OrderStatusEnum Status { get; private set; }
         public ICollection<OrderLine> OrderLines { get; private set; }
+
+        public void CreateOrder(List<CreateOrderDto> createOrderDtos)
+        {
+
+            foreach (var item in createOrderDtos)
+            {
+                if (item.quantity < 1)
+                {
+                    throw new ArgumentException("Quantity must be greater than 0");
+                }
+
+                OrderLine orderLine = new(
+                    Guid.NewGuid(),
+                    Id,
+                    item.productId,
+                    item.quantity,
+                    new(item.amaunt, Currency.FromCode(item.currency))
+                    );
+                OrderLines.Add(orderLine);
+            }
+        }
+
+        public void RemoveOrderLine(Guid orderLineId)
+        {
+            var orderLine = OrderLines.FirstOrDefault(p => p.Id == orderLineId);
+            if(orderLine is null)
+            {
+                throw new ArgumentException("OrderLine that you want to delete could not be found");
+            }
+            OrderLines.Remove(orderLine);
+        }
     }
 }
+
