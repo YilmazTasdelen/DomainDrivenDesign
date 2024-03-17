f8ba5890a6f855838a85ceca74db0f707ba8ff61
yilmaz.tasdelen <yilmaztasdelen1994@gmail.com>
2024-03-17 18:44:49 +0300

    Interfaces added.

---
+++ DomainDrivenDesign.Domain/Orders/IOrderRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static DomainDrivenDesign.Domain.Orders.Order;
+
+namespace DomainDrivenDesign.Domain.Orders
+{
+    public interface IOrderRepository
+    {
+        Task Create(List<CreateOrderDto> createOrderDtos, CancellationToken cancellationToken = default);
+
+        Task<List<Order>> GetAllAsync(CancellationToken cancellationToken = default);
+
+    }
+}
---
+++ DomainDrivenDesign.Domain/Products/IProductRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainDrivenDesign.Domain.Products
+{
+    public interface IProductRepository
+    {
+        Task CreateAsync(string name, int quantity, decimal amaunt, string currency, Guid categoryId,CancellationToken cancellationToken = default);
+
+        Task<List<Product>> GetAllAsync(CancellationToken cancellationToken = default);
+    }
+}
