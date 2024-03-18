d112192aa0e4db0a689b09f26522e8672acb403a
yilmaz.tasdelen <yilmaztasdelen1994@gmail.com>
2024-03-18 23:59:05 +0300

    .

---
+++ DomainDrivenDesign.Application/Features/User/CreateUser/CreateUserCommand.cs
@@ -0,0 +1,56 @@
+﻿using DomainDrivenDesign.Domain.Abstractions;
+using DomainDrivenDesign.Domain.Users;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DomainDrivenDesign.Application.Features.User.CreateUser
+{
+    public sealed record CreateUserCommand(
+        string name,
+        string email,
+        string password,
+        string country,
+            string city,
+            string street,
+            string postalCode,
+            string fullAddress
+        ) :IRequest;
+
+
+
+    internal sealed class CreateUserCommandHandler : IRequestHandler<CreateUserCommand>
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IUnitfWork unitfWork;
+        public CreateUserCommandHandler(IUserRepository userRepository)
+        {
+            this.userRepository = userRepository;
+        }
+
+        public async  Task Handle(CreateUserCommand request,CancellationToken cancellationToken)
+        {
+            await userRepository.CreateAsync(
+                request.name,
+                request.email,
+                request.password,   
+                request.country,    
+                request.city,   
+                request.street, 
+                request.postalCode, 
+                request.fullAddress,
+                cancellationToken
+                    
+                );
+
+            await unitfWork.SaveChangesAsync( cancellationToken );  
+        }
+
+
+    }
+
+
+}
